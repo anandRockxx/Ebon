cc1a36eeb81ad72d7b31a88dc7cb2cba91573a83
jekyllx <anand.pal2050@gmail.com>
2017-11-08 10:25:58 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-
 // preloader
-$(window).on('load',function(){
-  $('.spinner').fadeOut('slow',function(){
-    $('.preloader').remove();
-    $("nav").addClass('fixed-top');
-  });
+$(window).on('load', function() {
+    $('.spinner').fadeOut('slow', function() {
+        $('.preloader').remove();
+        $("nav").addClass('fixed-top');
+    });
 });
 
 
 // tab
-$('#myTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
+$('#myTab a').click(function(e) {
+    e.preventDefault()
+    $(this).tab('show')
 });
 
 
@@ -21,11 +20,11 @@ $width = $(window).width();
 if ($width > 480) {
 
 
-$(window).scroll(function() {
-   $scroll = $(window).scrollTop();
-   if ($scroll < 400 ) {
-      $(".sidebar h3").css("padding-top",100+$scroll+"px");
-   }
-});
+    $(window).scroll(function() {
+        $scroll = $(window).scrollTop();
+        if ($scroll < 400) {
+            $(".sidebar h3").css("padding-top", 100 + $scroll + "px");
+        }
+    });
 
 }
